14504476d1ef136245e7198ae0d473171681d0ba
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-02-03 23:38:23 -0300

    RP-317

---
+++ database/migrations/2018_10_18_231619_create_places_tables.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Repete em Informate
+ */
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesTables extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        
+        Schema::create(
+            'places', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('name');
+                $table->timestamps();
+                $table->softDeletes();
+            }
+        );
+        
+        Schema::create(
+            'placeables', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('placeable_id');
+                $table->string('placeable_type', 255);
+
+                $table->unsignedInteger('place_id')->nullable();
+                // $table->foreign('place_id')->references('id')->on('places');
+                $table->timestamps();
+                $table->softDeletes();
+            }
+        );
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('placeables');
+        Schema::dropIfExists('places');
+    }
+
+}
